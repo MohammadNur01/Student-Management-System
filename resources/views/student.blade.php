89ba7778b492a7784d6eb6af8ca0572e920e95b8
MohammadNur01 <mohammadnurrossifumi@gmail.com>
2022-04-08 15:49:38 +0700

    menampilkan data

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -11,33 +11,119 @@
     <title>Student Management System</title>
   </head>
   <body>
+    @include('navbar')
+    <div class="row  justify-content-center">
+        <div class="header">
+            <h1>Student Management System</h1>
+        </div>
+    </div>
     @if($layout == 'index')
-        <div class="container-fluid">
-            <section class="col">
-                @include("studentlist")
-            </section>
-            <section class="col"></section>
+        <div class="container-fluid mt-4">
+            <div class="row">
+                <section class="col-md-7">
+                    @include("studentlist")
+                </section>
+                <section class="col-md-5">
+                    
+                </section>
+            </div>
         </div>
     @elseif($layout == 'create')
-        <div class="container-fluid">
-            <section class="col">
-                @include("studentlist")
-            </section>
-            <section class="col"></section>
+        <div class="container-fluid mt-4">
+            <div class="row">
+                <section class="col-md-7">
+                    @include("studentlist")
+                </section>
+                <section class="col-md-5">
+                    <div class="card mb-3">
+                        <img src="https://www.intel.com/content/dam/www/central-libraries/us/en/images/adobestock-370985958.jpeg.rendition.intel.web.576.324.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Enter the new informations of the new student</h5>
+                            <form action="{{ url('/store') }}" method="post">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <label class="form-label">CNE</label>
+                                    <input type="text" class="form-control" id="cne" name="cne" placeholder="Enter cne">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">First Name</label>
+                                    <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Enter the first name">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">Second Name</label>
+                                    <input type="text" class="form-control" id="secondName" name="secondName" placeholder="Enter the second name">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">Age</label>
+                                    <input type="text" class="form-control" id="age" name="age" placeholder="Enter the age">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label class="form-label">Speciality</label>
+                                    <input type="text" class="form-control" id="speciality" name="speciality" placeholder="Enter the speciality">
+                                </div>
+                                <input type="submit" class="btn btn-info" value="Save">
+                                <input type="submit" class="btn btn-warning" value="Reset">
+                            </form>
+                        </div>
+                    </div>
+                    
+                    
+                </section>
+            </div>
         </div>
     @elseif($layout == 'show')
-        <div class="container-fluid">
-            <section class="col">
-                @include("studentlist")
-            </section>
-            <section class="col"></section>
+        <div class="container-fluid mt-4">
+            <div class="row">
+                <section class="col-md-7">
+                    @include("studentlist")
+                </section>
+                <section class="col-md-5">
+                    
+                </section>
+            </div>
         </div>
     @elseif($layout == 'edit')
-    <div class="container-fluid">
-        <section class="col">
-            @include("studentlist")
-        </section>
-        <section class="col"></section>
+    <div class="container-fluid mt-4">
+        <div class="row">
+            <section class="col-md-7">
+                @include("studentlist")
+            </section>
+            <section class="col-md-5">
+
+                <div class="card mb-3">
+                    <img src="https://www.freestudy.com/wp-content/uploads/2019/11/Education.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Update informations of student</h5>
+                      <form action="{{ url('/update/' . $student->id) }}" method="post">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label class="form-label">CNE</label>
+                            <input value="{{ $student->cne }}" type="text" class="form-control" id="cne" name="cne" placeholder="Enter cne">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label class="form-label">First Name</label>
+                            <input value="{{ $student->firstName }}" type="text" class="form-control" id="firstName" name="firstName" placeholder="Enter the first name">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label class="form-label">Second Name</label>
+                            <input value="{{ $student->secondName }}" type="text" class="form-control" id="secondName" name="secondName" placeholder="Enter the second name">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label class="form-label">Age</label>
+                            <input value="{{ $student->age }}" type="text" class="form-control" id="age" name="age" placeholder="Enter the age">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label class="form-label">Speciality</label>
+                            <input value="{{ $student->speciality }}" type="text" class="form-control" id="speciality" name="speciality" placeholder="Enter the speciality">
+                        </div>
+                        <input type="submit" class="btn btn-info" value="Update">
+                        <input type="submit" class="btn btn-warning" value="Reset">
+                    </form>
+                    </div>
+                </div>
+               
+            </section>
+        </div>
     </div>
     @endif
     <!-- Optional JavaScript; choose one of the two! -->
--- resources/views/studentlist.blade.php
+++ resources/views/studentlist.blade.php
@@ -1,29 +1,41 @@
+<div class="card mb-3">
+  <img width="400" src="https://www.eurodiaconia.org/wordpress/wp-content/uploads/2017/01/education.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">List Student</h5>
+    <p class="card-text">You can find here all the informations about students in system.</p>
+  </div>
+  <table class="table table-striped">
+    <thead class="thead-light">
+      <tr>
+        <th scope="col">CNE</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Second Name</th>
+        <th scope="col">Age</th>
+        <th scope="col">Speciality</th>
+        <th scope="col">Operations</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($students as $student)
+            <tr>
+                <th>{{ $student->cne }}</th>
+                <td>{{ $student->firstName }}</td>
+                <td>{{ $student->secondName }}</td>
+                <th>{{ $student->age }}</th>
+                <td>{{ $student->speciality }}</td>
+                <td>
+                    <a href="" class="btn btn-sm btn-info">Show</a>
+                    <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                    <a href="" class="btn btn-sm btn-danger">Delete</a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+  </table>
 
-    <table class="table">
-        <thead class="thead-light">
-          <tr>
-            <th scope="col">CNE</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Second Name</th>
-            <th scope="col">Age</th>
-            <th scope="col">Speciality</th>
-            <th scope="col">Operations</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach($students as $student)
-                <tr>
-                    <th>{{ $student->cne }}</th>
-                    <td>{{ $srudent->firstName }}</td>
-                    <td>{{ $student->secondName }}</td>
-                    <th>{{ $student->age }}</th>
-                    <td>{{ $student->speciality }}</td>
-                    <td>
-                        <a href="" class="btn btn-sm btn-info">Show</a>
-                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Edit</a>
-                        <a href="" class="btn btn-sm btn-danger">Delete</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-      </table>
+
+</div>
+
+
+
+   
